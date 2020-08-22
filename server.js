ed57b324ca67f9459f633baadf4560a2b672f3dc
TuxFan77 <briancross@shaw.ca>
2020-08-21 23:44:50 -0600

    socket io initial setup

---
+++ server.js
@@ -0,0 +1,17 @@
+const PORT = process.env.PORT || 3000;
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+
+io.on("connection", socket => {
+  console.log("Client connected");
+});
