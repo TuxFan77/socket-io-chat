77d8ed775aa39acd4ee3a309c3c3a25bdadca257
TuxFan77 <briancross@shaw.ca>
2020-08-22 09:50:17 -0600

    added chat form submit handler

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,11 @@
 const socket = io();
 
-socket.on("message", console.log);
+const chatForm = document.getElementById("chat-form");
+
+chatForm.addEventListener("submit", e => {
+  e.preventDefault();
+  const { msg } = e.target.elements;
+  socket.emit("chat", msg.value);
+  msg.value = "";
+  msg.focus();
+});
