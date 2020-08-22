4dca61b49e061907894e74f91e0b4d9e9d1df27a
TuxFan77 <briancross@shaw.ca>
2020-08-22 09:52:03 -0600

    emit welcome, connect, and disconnect messages

--- server.js
+++ server.js
@@ -13,7 +13,22 @@ app.use(express.static(path.join(__dirname, "public")));
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
 
 io.on("connection", socket => {
-  console.log("Client connected. Socket ID:", socket.id);
+  // Send message to the client that just connected
+  socket.emit("message", "Welcome to ChitChat!");
 
-  io.emit("message", "Welcome to ChitChat.");
+  // Broadcast to all clients except the one that just connected
+  socket.broadcast.emit(
+    "message",
+    `Client ${socket.id} has connected to the chat.`
+  );
+
+  // When client disconnects, send a message to all clients.
+  // Since the socket object has disconnected we have to use io.emit.
+  // This broadcasts a message to every socket connection.
+  socket.on("disconnect", () => {
+    console.log(`Client ${socket.id} disconnected.`);
+    io.emit("message", `Client ${socket.id} has left the chat.`);
+  });
+
+  socket.on("chat", console.log);
 });
